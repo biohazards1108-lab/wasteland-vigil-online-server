a3845a69213767865d8aea45d813c947320b42e3
biohazards1108-lab <biohazards1108@gmail.com>
2026-05-25 19:13:23 -0400

    Update player.js

--- src/schema/player.js
+++ src/schema/player.js
@@ -2,10 +2,20 @@ const schema = require("@colyseus/schema");
 const { Schema, type } = schema;
 
 class Player extends Schema {
-  @type("string") id;
-  @type("number") x = 0;
-  @type("number") y = 0;
-  @type("number") hp = 100;
+  constructor() {
+    super();
+    this.id = "";
+    this.x = 0;
+    this.y = 0;
+    this.hp = 100;
+  }
 }
+
+schema.defineTypes(Player, {
+  id: "string",
+  x: "number",
+  y: "number",
+  hp: "number"
+});
 
 module.exports = { Player };
